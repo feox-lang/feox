3cfbaf3736f2fde62af4faad45f4458d222810bf
tejtex <tejtex.dev@gmail.com>
2026-04-28 14:43:49 +0200

    feat:

--- tests/cases.py
+++ tests/cases.py
@@ -4,9 +4,13 @@ CASES_DIR = (Path(__file__).parent / "cases").resolve()
 CACHE_DIR = (Path(__file__).parent / "cache").resolve()
 CACHE_DIR.mkdir(exist_ok=True)
 
-FEOX_PATH = Path(__file__).parent.parent / "target" / "debug" / "feox"
+FEOX_PATH = Path(__file__).parent.parent / "target"
+release = FEOX_PATH / "release" / "feox"
+debug = FEOX_PATH / "debug" / "feox"
+
+FEOX_PATH = release if release.exists() else debug
 
 CASES = sorted(
-    (p.parent.resolve() for p in Path("tests/cases").rglob("test.fe")),
+    (p.parent.resolve() for p in CASES_DIR.rglob("test.fe")),
     key=lambda p: str(p)
 )
